2a43ff93c5c9c1ee9376240f5ec2d8528af2f9f5
Mohammed OK <ok971@inlumine.ual.es>
2026-04-17 15:01:48 +0000

    PR completos

--- P/asigna.py
+++ P/asigna.py
@@ -1,2 +1,21 @@
+Pr0
 a=1
 b=2
+
+Pr1
+print("Hola mundo")
+
+nombre = "Moha"
+edad = 22
+print(nombre, edad)
+
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+
